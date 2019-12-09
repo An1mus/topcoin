7cf4aa71877eddb491d42e23ccf0d7b6c004cebf
an1mus <1stallways@gmail.com>
2019-12-09 19:52:46 +0200

    Bootstrap added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Dashboard from "../dashboard";
 
 const App = () => {
 	return (
 		<>
 			<h1>The app is ready to go</h1>
+			<Dashboard />
 		</>
 	);
 };
