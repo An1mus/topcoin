8e2486ea0dfe94298bd7037dfcbec248d74f0b95
an1mus <1stallways@gmail.com>
2019-12-09 21:39:39 +0200

    Routing added, NODE_PATH set ot src/

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {Router, Route} from 'react-router-dom';
+
 import './App.css';
-import Dashboard from "../dashboard";
+
+import history from 'common/history';
+
+import Dashboard from 'components/dashboard';
+import Liquidity from 'components/liquidity';
 
 const App = () => {
 	return (
 		<div className={'container'}>
 			<h1>The app is ready to go</h1>
-			<Dashboard />
+
+			<Router history={history}>
+				<Route exact path={'/'} component={Dashboard}/>
+				<Route exact path={'/liquidity'} component={Liquidity}/>
+			</Router>
 		</div>
 	);
 };
