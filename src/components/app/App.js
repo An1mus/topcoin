7d837a310984116ac034fb129c56053274d1e762
an1mus <1stallways@gmail.com>
2019-12-10 15:40:27 +0200

    Service with middleware call prepared

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,8 +5,6 @@ import './App.css';
 
 import history from 'common/history';
 
-import getCoinData from 'services/cmc';
-
 import Dashboard from 'components/dashboard';
 import Liquidity from 'components/liquidity';
 
@@ -14,8 +12,6 @@ const App = () => {
 	return (
 		<div className={'container'}>
 			<h1>The app is ready to go</h1>
-
-			<button onClick={() => {getCoinData()}}>Data get</button>
 
 			<Router history={history}>
 				<Route exact path={'/'} component={Dashboard}/>
