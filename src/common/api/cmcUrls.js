435628ba54d43428163bdd4d6aa5612a76f7e423
an1mus <1stallways@gmail.com>
2019-12-10 15:37:55 +0200

    Alowed cors for the middleware

--- src/common/api/cmcUrls.js
+++ src/common/api/cmcUrls.js
@@ -1,19 +1,11 @@
 /**
  * CoinMarketCap API endpoints and headers
  * * * * * * * * * * * * * * * NOTE * * * * * * * * * * * * * * *
- * The API KEY is openly supplied only for the testing.
- * The key provided is a sandbox key, but the only difference is,
- * the API domain.
+ * The API endpoints are provided through the middleware, that is also committed to this repo.
+ * Such structure is necessary due to the fact, that new CMC API restricts front-end calls.
  */
 
-const API_KEY = 'c9f5efce-aa76-4376-8a70-c93924af1b62';
-const CMC_API_URL = 'https://sandbox-api.coinmarketcap.com/v1/';
-const CMC_API_URL_LISTING = `${CMC_API_URL}cryptocurrency/listings/latest`; //coin listing for the latest data
-const CMC_HEADERS = {
-	// Usually I would put it server side, and use Node middleware.
-	// But this is a sandbox key, so it should be okay.
-	'X-CMC_PRO_API_KEY': API_KEY,
-	'Accept': 'application/json'
-};
+const CMC_API_URL = 'http://localhost:5000/';
+const CMC_API_URL_LISTING = `${CMC_API_URL}cmc`; //coin listing for the latest data
 
-export {CMC_API_URL_LISTING, CMC_HEADERS};
+export {CMC_API_URL_LISTING};
--- src/services/cmc/index.js
+++ src/services/cmc/index.js
@@ -1,17 +1,12 @@
 import axios from 'axios';
 
-import {CMC_API_URL_LISTING, CMC_HEADERS} from 'common/api/cmcUrls';
+import {CMC_API_URL_LISTING} from 'common/api/cmcUrls';
 
 const getCoinData = async () => {
 	let response;
 
 	try {
-		response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-			headers: {
-				'X-CMC_PRO_API_KEY': 'c9f5efce-aa76-4376-8a70-c93924af1b62',
-				'Accept': 'application/json; charset=utf-8'
-			},
-		});
+		response = await axios.get(CMC_API_URL_LISTING);
 	} catch (e) {
 		return console.error(e);
 	}
