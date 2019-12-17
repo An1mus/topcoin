71b44f346da237677bcee0f89e6e99ef15068884
an1mus <1stallways@gmail.com>
2019-12-17 15:21:37 +0200

    Removing outdated API key after transfering it to the hidden config

--- server/config/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Private API key, not pushed to git
- * @type {{"X-CMC_PRO_API_KEY": string}}
- */
-const API_KEY_HEADER = {
-	'X-CMC_PRO_API_KEY': '0c820ad8-b596-4012-96b8-85cf946b3288'
-};
-
-export {API_KEY_HEADER};
